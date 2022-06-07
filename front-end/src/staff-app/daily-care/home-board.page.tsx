6c3438a8270d260ffc12a1ceec2ef16601f40605
rahulsg1 <rahulsg1508@gmail.com>
2022-06-07 12:05:00 +0530

    solution

--- front-end/src/staff-app/daily-care/home-board.page.tsx
+++ front-end/src/staff-app/daily-care/home-board.page.tsx
@@ -11,29 +11,51 @@ import { StudentListTile } from "staff-app/components/student-list-tile/student-
 import { ActiveRollOverlay, ActiveRollAction } from "staff-app/components/active-roll-overlay/active-roll-overlay.component"
 
 export const HomeBoardPage: React.FC = () => {
-  const [isRollMode, setIsRollMode] = useState(false)
+  const [isRollMode, setIsRollMode] = useState(false as any)
+  const [searchText, setSearchText] = useState("")
+  const [sort, setSort] = useState("")
   const [getStudents, data, loadState] = useApi<{ students: Person[] }>({ url: "get-homeboard-students" })
+  const [sortBy, setSortBy] = useState("asc")
+  const [sortByName, setSortByName] = useState("first_name")
+  const [text, setText] = useState("")
+  const [roleStatus, setRoleStatus] = useState({})
 
   useEffect(() => {
-    void getStudents()
-  }, [getStudents])
+    void getStudents({ sortBy, sortByName, text })
+  }, [sortBy, sortByName, text])
 
   const onToolbarAction = (action: ToolbarAction) => {
     if (action === "roll") {
       setIsRollMode(true)
+    } else if (action === "sort") {
+      setSort((sort == "" || sort == "decrease") ? "increase" : "decrease")
     }
+  }
+
+  const onChangeSortName = (value: string) => {
+    setSortByName(value)
   }
 
   const onActiveRollAction = (action: ActiveRollAction) => {
     if (action === "exit") {
       setIsRollMode(false)
     }
+    if (action === "complete") {
+      setIsRollMode(false)
+    }
+  }
+
+  const onStateChange = (id: number, value: string) => {
+    let status: any = { ...roleStatus }
+    status[id] = value
+    console.log(value)
+    setRoleStatus(status)
   }
 
   return (
     <>
       <S.PageContainer>
-        <Toolbar onItemClick={onToolbarAction} />
+        <Toolbar onItemClick={onToolbarAction} sortByName={sortByName} onChangeSortName={onChangeSortName} setSearchText={setSearchText} />
 
         {loadState === "loading" && (
           <CenteredContainer>
@@ -41,11 +63,22 @@ export const HomeBoardPage: React.FC = () => {
           </CenteredContainer>
         )}
 
-        {loadState === "loaded" && data?.students && (
+        {loadState === "loaded" && data?.students && data?.students?.length > 0 && (
           <>
-            {data.students.map((s) => (
-              <StudentListTile key={s.id} isRollMode={isRollMode} student={s} />
-            ))}
+            {data.students.sort((a, b) => {
+              if (sort === "increase") {
+                return (a.first_name + a.last_name < b.first_name + b.first_name ? -1 : 1)
+              } else if (sort === "decrease") {
+                return (a.first_name + a.last_name > b.first_name + b.first_name ? -1 : 1)
+              } else {
+                return 1
+              }
+            }).filter((s) => (s.first_name + " " + s.last_name).toLowerCase().includes(searchText.toLowerCase())).map((s) => {
+              return (
+                <StudentListTile key={s.id} isRollMode={isRollMode} student={s} onStateChange={onStateChange} />
+              )
+            }
+            )}
           </>
         )}
 
@@ -55,7 +88,7 @@ export const HomeBoardPage: React.FC = () => {
           </CenteredContainer>
         )}
       </S.PageContainer>
-      <ActiveRollOverlay isActive={isRollMode} onItemClick={onActiveRollAction} />
+      <ActiveRollOverlay isActive={isRollMode} onItemClick={onActiveRollAction} roleStatus={roleStatus} />
     </>
   )
 }
@@ -63,13 +96,22 @@ export const HomeBoardPage: React.FC = () => {
 type ToolbarAction = "roll" | "sort"
 interface ToolbarProps {
   onItemClick: (action: ToolbarAction, value?: string) => void
+  sortByName: string
+  onChangeSortName: (value: string) => void
+  setSearchText: (search: string) => void
 }
 const Toolbar: React.FC<ToolbarProps> = (props) => {
-  const { onItemClick } = props
+  const { onItemClick, sortByName, onChangeSortName, setSearchText } = props
   return (
     <S.ToolbarContainer>
       <div onClick={() => onItemClick("sort")}>First Name</div>
-      <div>Search</div>
+      <div>
+        {/* <select value={sortByName} onChange={(e) => onChangeSortName(e.target.value)}>
+          <option value="first_name">First Name</option>
+          <option value="last_name">Last Name</option>
+        </select> */}
+        <input type="text" placeholder="Search..." onChange={(e) => setSearchText(e.target.value)} />
+      </div>
       <S.Button onClick={() => onItemClick("roll")}>Start Roll</S.Button>
     </S.ToolbarContainer>
   )
